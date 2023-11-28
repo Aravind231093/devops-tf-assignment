c1adbae291e69b713613398305d425317cae941f
Aravind231093 <124013013+Aravind231093@users.noreply.github.com>
2023-11-28 12:14:14 +0800

    Update lambda.tf

--- app/lambda.tf
+++ app/lambda.tf
@@ -54,4 +54,10 @@ data "aws_iam_policy_document" "this" {
 
 resource "aws_lambda_function" "this" {
   # TO FILL IN
+lambda_zip_name = "${path.module}/lambda.${random_string.r.result}.zip"
+function_name   = var.name
+role                           = aws_iam_role.lambda_role.arn
+handler                        = "hello-nodejs.lambda_handler"
+runtime                        = "python3.8"
+depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
